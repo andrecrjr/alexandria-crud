cde5d0ecac007e093805d948712e6cf0c1a4e1a1
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-05-31 01:32:47 -0300

    feature: user guard to save nest roue

--- backend/alexandria-api/src/auth/auth.module.ts
+++ backend/alexandria-api/src/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 import { jwtConstants } from 'src/constants';
 
@@ -17,7 +16,7 @@ import { jwtConstants } from 'src/constants';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
+  providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
--- backend/alexandria-api/src/users/users.controller.ts
+++ backend/alexandria-api/src/users/users.controller.ts
@@ -1,5 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -7,5 +15,12 @@ export class UsersController {
   @Post()
   async create(@Body() data: { username: string; password: string }) {
     return this.usersService.create(data.username, data.password);
+  }
+
+  // user routes protection
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
